46c78f8528b390c3afb8e7cf753f607c1bdfed76
Juan Manuel Bottarelli Frei <juanmanuelfrei@gmail.com>
2025-07-31 01:14:50 -0300

    riu-challange: first approach of architecture, local db created, angular material added

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,18 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'heroes',
+    loadComponent: () =>
+      import('./modules/heroes/heroes').then((c) => c.Heroes),
+  },
+  {
+    path: '',
+    redirectTo: 'heroes',
+    pathMatch: 'full',
+  },
+  {
+    path: '**',
+    redirectTo: 'heroes',
+  },
+];
---
+++ src/app/shared/components/loading/loading.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'riu-loading',
+  imports: [],
+  templateUrl: './loading.html',
+  styleUrl: './loading.scss',
+})
+export class Loading {}
